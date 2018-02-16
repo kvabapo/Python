d14ae2c4c30b98386d2c29c9934d3374039fa3e0
kvabapo <36245725+kvabapo@users.noreply.github.com>
2018-02-16 18:23:52 +0800

    Add files via upload

---
+++ datastructures.py
@@ -0,0 +1,35 @@
+
+
+list=["l1","l2","l3"]
+
+for l in list:
+    print(l)
+
+print(list[1])
+
+tuple=(1,2,3)
+
+for t in tuple:
+    print(t)
+
+print(tuple[0])
+
+dict={"food1":"donut","food2":"sandwich","food3":"cake"}
+
+print(dict["food2"])
+
+for d in dict:
+    print(d)
+
+for d, value in dict.items():
+    print (d, value)
+
+
+#http://effbot.org/zone/python-list.htm
+#https://sites.google.com/site/usfcomputerscience/basic-programming
+
+
+#filepath="\Folder\Subfolder"
+#filename=open(filepath,r)
+#filecontents=read(filename)
+#print(filecontents)
